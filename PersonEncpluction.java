a77c13d8961c3b807359c7b9f4f11c095a0ed17c
Pawan G Tekale <121091609+Pawangtekale@users.noreply.github.com>
2025-06-13 12:34:48 +0530

    Add files via upload

---
+++ PersonEncpluction.java
@@ -0,0 +1,11 @@
+class PersonEncpluction
+{
+	public static void main(String[]args){
+		Person p2=new Person();
+		p2.setFirstName("Pawan");
+		System.out.println(p2.getFirstName());
+		p2.setLastName("Tekale");
+		System.out.println(p2.getLastName());
+		
+	}
+}
